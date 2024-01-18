38897b451764cdf3b8584940e2d04510665e05f7
darren wang <darren@aptoslabs.com>
2024-01-17 17:13:40 -0800

    update geotargetly

--- frontend/src/utils/GeoTargetly.tsx
+++ frontend/src/utils/GeoTargetly.tsx
@@ -9,10 +9,10 @@ export const GeoTargetly = () => {
       dangerouslySetInnerHTML={{
         __html: `
         (function(g,e,o,t,a,r,ge,tl,y){
-            t=g.getElementsByTagName(o)[0];y=g.createElement(e);y.async=true;
-            y.src='https://g9904216750.co/gb?id=-NkwFfbPXy6zrJ3lNunn&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
-            t.parentNode.insertBefore(y,t);
-            })(document,'script','head');
+          t=g.getElementsByTagName(o)[0];y=g.createElement(e);y.async=true;
+          y.src='https://g9904216750.co/gb?id=-NkwFHOr-budQ-E4x08r&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
+          t.parentNode.insertBefore(y,t);
+          })(document,'script','head');
         `,
       }}
     />
